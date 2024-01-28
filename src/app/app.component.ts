f2437ba9aff5f83a9d414423e6eb107f3f09835c
Petru Grozea <petrugrozea@gmail.com>
2024-01-28 05:09:36 +0200

    feat: test unsplash images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { UnsplashService } from './services/unsplash.service';
+import { AsyncPipe, NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [NgIf, NgFor, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  protected images$ = this.unsplashService.getSavedPhotos(1);
+
+  constructor(private unsplashService: UnsplashService) {}
+}
